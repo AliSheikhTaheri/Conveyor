9dd6e9d70351d80c132a1637197932b9f4c21979
ataheri <ataheri@thisiszone.com>
2014-09-11 15:28:53 +0100

    Added Damp DataType Converter

---
+++ WebUI/WebUI/Infrastructure/DataTypeConverters/DampDataTypeConverter.cs
@@ -0,0 +1,60 @@
+﻿namespace WebUI.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web;
+    using System.Xml.Linq;
+    using DigibizTree;
+    using Umbraco.Core.Models;
+    using DampItem = DAMP.PropertyEditorValueConverter.DAMP_Item;
+    using DampModel = DAMP.PropertyEditorValueConverter.Model;
+
+    public class DampDataTypeConverter : BaseContentManagement, IDataTypeConverter
+    {
+        public void Export(Property property, XElement propertyTag, Dictionary<int, ObjectTypes> dependantNodes)
+        {
+            if (property.Value != null && !string.IsNullOrWhiteSpace(property.Value.ToString()))
+            {
+                var items = new DampModel(property.Value.ToString());
+                var guidList = new List<Guid>();
+
+                if (items.Any)
+                {
+                    foreach (var item in items)
+                    {
+                        guidList.Add(Services.MediaService.GetById(item.Id).Key);
+
+                        if (!dependantNodes.ContainsKey(item.Id))
+                        {
+                            dependantNodes.Add(item.Id, ObjectTypes.Media);
+                        }
+                    }
+                }
+
+                propertyTag.Value = string.Join(",", guidList);
+            }
+        }
+
+        public string Import(XElement propertyTag)
+        {
+            var result = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(propertyTag.Value))
+            {
+                var listOfGuid = propertyTag.Value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                var listOfIds = new List<string>();
+
+                foreach (var guid in listOfGuid)
+                {
+                    listOfIds.Add(Services.MediaService.GetById(new Guid(guid)).Id.ToString());
+                }
+
+                var xml = DigibizMediaHelper.GetXML(listOfIds.ToArray());
+
+                result = HttpUtility.HtmlDecode(xml.ToString());
+            }
+
+            return result;
+        }
+    }
+}
