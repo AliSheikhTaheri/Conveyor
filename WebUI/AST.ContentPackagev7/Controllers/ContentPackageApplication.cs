c24a3af9d8bfd791ce6557de38f31a60ad6962d1
Robin Herd <rherd@thisiszone.com>
2014-12-08 16:37:35 +0000

    initial commit for new branch; new umbraco v7 projects added to solution

---
+++ WebUI/AST.ContentPackagev7/Controllers/ContentPackageApplication.cs
@@ -0,0 +1,11 @@
+﻿namespace AST.ContentPackagev7.Controllers
+{
+    using umbraco.businesslogic;
+    using umbraco.interfaces;
+
+    // TODO change the icon
+    [Application("ContentPackage", "ContentPackage", "icon-car", 15)]
+    public class ContentPackageApplication : IApplication
+    {
+    }
+}
---
+++ WebUI/AST.ContentPackagev7/Controllers/ContentPackageTreeController.cs
@@ -0,0 +1,23 @@
+﻿namespace AST.ContentPackagev7.Controllers
+{
+    using System.Net.Http.Formatting;
+
+    using Umbraco.Web.Models.Trees;
+    using Umbraco.Web.Mvc;
+    using Umbraco.Web.Trees;
+
+    [PluginController("CustomSection")]
+    [Tree("ContentPackage", "ContentPackageTree", "AST Content Package", iconClosed: "icon-doc")]
+    public class ContentPackageTreeController : TreeController
+    {
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
