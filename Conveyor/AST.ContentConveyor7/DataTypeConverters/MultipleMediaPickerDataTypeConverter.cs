0b1a38997edab794954480c7cee8d89fb6bc53a7
Jose Marcenaro <josem@tercerplaneta.com>
2016-10-21 11:49:33 -0300

    Add support for MultipleMediaPicker data type (v7)

---
+++ Conveyor/AST.ContentConveyor7/DataTypeConverters/MultipleMediaPickerDataTypeConverter.cs
@@ -0,0 +1,62 @@
+﻿namespace AST.ContentConveyor7.DataTypeConverters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    using AST.ContentConveyor7;
+    using AST.ContentConveyor7.Enums;
+    using AST.ContentConveyor7.Utilities;
+
+    using Umbraco.Core.Models;
+
+    public class MultipleMediaPickerDataTypeConverter : BaseContentManagement, IDataTypeConverter
+    {
+        public void Export(Property property, XElement propertyTag, Dictionary<int, ObjectTypes> dependantNodes)
+        {
+            if (property.Value != null && !string.IsNullOrWhiteSpace(property.Value.ToString()))
+            {
+                var ids = property.Value.ToString().Split(',').Select(s => int.Parse(s));
+                List<string> guids = new List<string>(ids.Count());
+
+                foreach (int id in ids)
+                {
+                    var media = Services.MediaService.GetById(id);
+                    if (media != null)
+                    {
+                        guids.Add(media.Key.ToString());
+                        if (!dependantNodes.ContainsKey(media.Id))
+                        {
+                            dependantNodes.Add(media.Id, ObjectTypes.Media);
+                        }
+                    }
+                }
+                propertyTag.Value = String.Join(",", guids);
+            }
+        }
+
+        public string Import(XElement propertyTag)
+        {
+            var result = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(propertyTag.Value))
+            {
+                var guids = propertyTag.Value.Split(',').Select(s => new Guid(s));
+                List<string> ids = new List<string>(guids.Count());
+
+                foreach (Guid guid in guids)
+                {
+                    var media = Services.MediaService.GetById(guid);
+                    if (media != null)
+                    {
+                        ids.Add(media.Id.ToString());
+                    }
+                }
+                result = String.Join(",", ids);
+
+            }
+            return result;
+        }
+    }
+}
